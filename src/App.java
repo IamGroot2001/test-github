a858f6d24c43219b57757faed643c4b98eb1a069
IamGroot2001 <daohoanh01637788488@gmai.com>
2021-12-23 14:30:06 +0700

    lan2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! HA");
+        System.out.println("Hello, World! HA 2345");
     }
 }
