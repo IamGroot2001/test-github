4df0d3dbf5e96663b5ae1e9796d8c910137e814f
IamGroot2001 <96557568+IamGroot2001@users.noreply.github.com>
2021-12-23 15:00:42 +0700

    12313

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! HA 2345");
+        System.out.println("Hello, World! HA 2345 12313");
     }
 }
