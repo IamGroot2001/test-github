b6ef238686343cc886e758597aa5adc79a282cc8
IamGroot2001 <daohoanh01637788488@gmail.com>
2021-12-23 15:09:01 +0700

    lan3

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! HA 2345 12313");
+        System.out.println("Dao Ho Anh");
     }
 }
